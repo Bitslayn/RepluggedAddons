6e443fa447606974081d8d52400758a390784da1
Federico <38290480+FedeIlLeone@users.noreply.github.com>
2024-06-17 13:43:15 +0200

    refactor: cleanup

--- plugins/btw.bitslayn.channelIcons/src/icons.ts
+++ plugins/btw.bitslayn.channelIcons/src/icons.ts
@@ -1,20 +1,33 @@
-﻿import { settings, webpack } from "replugged";
-import { IconData, Settings } from "./types";
+﻿import type React from "react";
+import { settings, webpack } from "replugged";
+import type { GenericIconProps, IconData, Settings } from "./types";
 
-export const config = await settings.init<Settings>("btw.bitslayn.channelIcons");
+const defaultSettings = {
+  coloredChannels: {},
+  changeChannelNames: true,
+  presetChannelIcons: true,
+  coloredChannelPills: true,
+  suggestedColors: [],
+  icons: [],
+} satisfies Partial<Settings>;
 
-async function fetchDataAndExtract(): Promise<IconData[]> {
+export const config = await settings.init<Settings, keyof typeof defaultSettings>(
+  "btw.bitslayn.channelIcons",
+  defaultSettings
+);
+
+async function fetchDataAndExtract(): Promise<IconData[] | undefined> {
   const url =
     "https://raw.githubusercontent.com/Bitslayn/RepluggedPlugins/main/assets/classicIcons.ts";
   try {
     // Fetch CSS file
-    const response: Response = await fetch(url);
-    const cssContent: string = await response.text();
+    const response = await fetch(url);
+    const cssContent = await response.text();
 
     const pattern = /\/\*(.+?)\*\/ {[\s\S]*?d: path\("(.*?)"\);\}/g;
 
-    const extractedData: IconData[] = [];
-    let match: RegExpExecArray | null;
+    const extractedData = [];
+    let match;
     // eslint-disable-next-line no-cond-assign
     while ((match = pattern.exec(cssContent)) !== null) {
       extractedData.push({ label: match[1], value: match[2] });
@@ -26,19 +39,28 @@ async function fetchDataAndExtract(): Promise<IconData[]> {
   }
 }
 
-const Icons: IconData[] = config.get("icons");
+const Icons = config.get("icons");
 
 config.set("icons", await fetchDataAndExtract());
 
-const UpdatedIcons = webpack.getBySource("www.w3.org/2000/svg", { all: true });
-const group1Array = [];
+interface ModernIconArray {
+  name: string;
+  matches: RegExpMatchArray[];
+}
+
+const UpdatedIcons = webpack.getBySource<Record<string, React.FC<GenericIconProps>>>(
+  "www.w3.org/2000/svg",
+  { all: true }
+);
+const group1Array: ModernIconArray[] = [];
+
 UpdatedIcons.forEach(iconObject => {
   const iconKey = Object.keys(iconObject).find(key => key.includes("Icon"));
   if (!iconKey) return;
   const iconValueString = iconObject[iconKey]?.toString?.();
   if (!iconValueString) return;
   const matches = [...iconValueString.matchAll(/,d:"([^"]*)"/g)];
-  if (matches.length) group1Array.push({ Name: iconKey, Matches: matches });
+  if (matches.length) group1Array.push({ name: iconKey, matches });
 });
 
-export { Icons, group1Array };
+export { group1Array, Icons };
